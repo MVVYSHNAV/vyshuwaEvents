0068b0303b674da629153a2bda346a3eb2d44d1f
vyshnav <vyshnavmv15@gmail.com>
2025-09-24 18:23:55 +0530

    fea: created core doctype event with festa

---
+++ vyshuwaops/vyshuwaops/doctype/festa_categories/test_festa_categories.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, It enables organizers to create and publish events, define venues, and set ticket categories, while attendees can browse events, book tickets, and receive confirmations with secure online payments.vyshnav and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestFestaCategories(FrappeTestCase):
+	pass
---
+++ vyshuwaops/vyshuwaops/doctype/festa_venue/festa_venue.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, It enables organizers to create and publish events, define venues, and set ticket categories, while attendees can browse events, book tickets, and receive confirmations with secure online payments.vyshnav and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Festa Venue", {
+// 	refresh(frm) {
+
+// 	},
+// });
