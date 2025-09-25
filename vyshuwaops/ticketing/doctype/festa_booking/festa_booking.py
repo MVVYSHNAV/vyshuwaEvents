3eba0be3502494daca4fba43b5f32afabda70bca
vyshnav <vyshnavmv15@gmail.com>
2025-09-25 11:26:57 +0530

    feat: ticket booking and cancell

--- vyshuwaops/ticketing/doctype/festa_booking/festa_booking.py
+++ vyshuwaops/ticketing/doctype/festa_booking/festa_booking.py
@@ -29,12 +29,4 @@ class FestaBooking(Document):
 			ticket.attende_name = attende.full_name
 			ticket.insert().submit()
 
-	def before_cancel(self):
-		self.ticket_Cancel()
-		
-	def ticket_cancel(self):
-		tickets_cancel_booking = frappe.db.get_all("Festa Ticket", {"booking": self.name }, pluck="name")
-
-		for ticket in tickets_cancel_booking:
-			ticket_doc = frappe.get_doc("Festa Ticket", ticket)
-			ticket_doc.cancel()
+	
